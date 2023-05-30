907848531b008f44ee198f5880627540fd389ff9
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-30 16:43:42 +0200

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="ms_container">
+
+
+
+    </div>
+</template>
+
+
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            projects: [],
+            url: "http://localhost:8000"
+        }
+    },
+    methods: {
+
+        getProjects() {
+            console.log("daje");
+
+
+            axios.get(`${this.url}/api/projects`)
+                .then(response => {
+                    console.log(response);
+                    this.projects = response.data.results;
+                })
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
+
+}
+
+</script>
