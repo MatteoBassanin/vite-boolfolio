182fa7413d409892b02168baa11451eac88226ac
MatteoBassanin <matteo.bassanin@gmail.com>
2023-06-07 16:50:11 +0200

    sending email

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <form @submit.prevent="sendForm()">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" v-model="email">
+            </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" v-model="name">
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Message</label>
+                <textarea class="form-control" id="message" v-model="message"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Send</button>
+        </form>
+    </div>
+</template>
+
+
+<script>
+
+import { store } from '../store';
+import axios from 'axios';
+export default {
+    name: 'ContactForm',
+    data() {
+        return {
+            store,
+            name: '',
+            email: '',
+            message: ''
+        }
+    },
+    methods: {
+        sendForm() {
+            axios.post(`${this.store.url}/api/contacts`,
+                {
+                    name: this.name,
+                    email: this.email,
+                    message: this.message,
+                }
+            ).then(response => {
+
+                console.log(response);
+
+            })
+        }
+    }
+
+}
+
+
+
+</script>
--- src/pages/AppShow.vue
+++ src/pages/AppShow.vue
@@ -1,13 +1,18 @@
 <template>
-    <h1>Roma</h1>
+    <h1>Contattami</h1>
+    <ContactForm></ContactForm>
 </template>
 
 
 
 
 <script>
+import ContactForm from '../components/ContactForm.vue';
 export default {
-    name: 'AppShow'
+    name: 'AppShow',
+    components: {
+        ContactForm,
+    }
 }
 
 </script>
